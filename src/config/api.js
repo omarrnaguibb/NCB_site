562921c328aa8ac6c4af2392e16062f6194100ea
omarrnaguibb <130666749+omarrnaguibb@users.noreply.github.com>
2026-06-23 00:23:47 +0300

    first commit

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 // export const api_route = "http://localhost:8080";
-export const api_route = "https://ccnnbb-ser.onrender.com";
+export const api_route = "https://lyb-cb-ser.onrender.com";
 
 export const socket = io(api_route, { autoConnect: true });
